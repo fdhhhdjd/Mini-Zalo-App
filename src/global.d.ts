551d9b1c79d5b4b8410c3183a5a8420d0f6eff64
Trần Thanh Phong <143787214+blackjack-ttp@users.noreply.github.com>
2023-11-04 11:22:39 +0700

    #36 feat: animation order success (#44)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,13 @@
 declare module "*.png" {
-  const content: string;
-  export default content;
+	const content: string;
+	export default content;
 }
 
 declare module "*.svg" {
-  const content: string;
-  export default content;
+	const content: string;
+	export default content;
+}
+declare module "*.gif" {
+	const content: string;
+	export default content;
 }
--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import success from "static/success.png";
+import thanksAnimation from "static/thanks.gif";
 import { Box, Header, Page, Text } from "zmp-ui";
 
 const SuccessPage: FC = () => {
@@ -14,9 +15,16 @@ const SuccessPage: FC = () => {
 					justifyContent="center"
 					className="bg-background h-full space-y-5"
 				>
+					<div className="pyro">
+						<div className="before"></div>
+						<div className="after"></div>
+					</div>
+					<div className="flex flex-col items-center space-y-2">
+						<img src={thanksAnimation} alt="" className="w-80 h-50 mx-auto mb-4" />
+					</div>
 					<img src={success} className="w-40 h-40" alt="success" />
-					<Text className="font-bold text-4xl">Thank you!</Text>
-					<Text className="text-1xl">Order was success</Text>
+					<Text className="font-bold text-6xl mb-2">Thank you!</Text>
+					<Text className="text-1xl mb-6 mt-10">Order was success</Text>
 				</Box>
 			</Page>
 		</>
