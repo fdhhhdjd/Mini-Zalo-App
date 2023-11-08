2adf78a28a683deffb781a1dc25d85c5a37c2cec
NguyenNam6689 <128495349+NguyenNam6689@users.noreply.github.com>
2023-11-08 14:27:49 +0700

    52 add tooltip all button (#72)
    * #51 fix: css and timeout tooltip
    
    * #51 fix: css and timeout tooltip
    
    * #51 fix: css and commit  tooltip
    
    * Revert "#51 fix: css and commit  tooltip"
    
    This reverts commit 803c0526b0a0c5f8031461139400308980ebb0d2.
    
    * #51 fix: css and commit  tooltip

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useRef, useState } from "react";
+import React, { FC, ReactNode, useRef } from "react";
 
 interface Props {
 	children: ReactNode;
@@ -8,26 +8,21 @@ interface Props {
 const ToolTip: FC<Props> = ({ children, tooltip }): JSX.Element => {
 	const tooltipRef = useRef<HTMLSpanElement>(null);
 	const container = useRef<HTMLDivElement>(null);
-	const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
+	const timeoutId = useRef<NodeJS.Timeout | null>(null);
 
 	return (
 		<div
 			ref={container}
 			onMouseEnter={() => {
 				if (!tooltipRef.current || !container.current) return;
-				const { width } = container.current.getBoundingClientRect();
 
-				tooltipRef.current.style.left = width / 2 + "px";
-				tooltipRef.current.style.transform = "translateX(-50%)";
-
-				if (timeoutId) clearTimeout(timeoutId);
-				const id = setTimeout(() => {
+				if (timeoutId.current) clearTimeout(timeoutId.current);
+				timeoutId.current = setTimeout(() => {
 					if (tooltipRef.current) tooltipRef.current.style.visibility = "hidden";
 				}, 2000);
-				setTimeoutId(id);
 			}}
 			onMouseLeave={() => {
-				if (timeoutId) clearTimeout(timeoutId);
+				if (timeoutId.current) clearTimeout(timeoutId.current);
 				if (tooltipRef.current) tooltipRef.current.style.visibility = "visible";
 			}}
 			className="group relative inline-block"
@@ -36,6 +31,7 @@ const ToolTip: FC<Props> = ({ children, tooltip }): JSX.Element => {
 			{tooltip ? (
 				<span
 					ref={tooltipRef}
+					style={{ left: "50%", transform: "translateX(-50%)" }}
 					className="invisible group-hover:visible opacity-0 group-hover:opacity-100 transition bg-[#767A7F] text-white p-1 rounded absolute top-10 mb-2 whitespace-nowrap"
 				>
 					{tooltip}
