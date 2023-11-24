a85fcf476e758e2106dbfa53ee554521b5b03450
Nguyễn Tiến Tài <63393170+fdhhhdjd@users.noreply.github.com>
2023-11-24 17:35:28 +0700

    #91 [ Frontend ] Remove Transition (#92)

--- src/components/RoutesWithTransition.tsx
+++ src/components/RoutesWithTransition.tsx
@@ -6,30 +6,21 @@ import ProfilePage from "pages/profile";
 import SearchPage from "pages/search";
 import SuccessPage from "pages/success";
 import React from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { Route, Routes } from "react-router-dom";
 
 const RoutesWithTransition: React.FC = () => {
-	const location = useLocation();
-
 	return (
-		<>
-			<TransitionGroup>
-				<CSSTransition key={location.pathname} classNames="router" timeout={{ enter: 400, exit: 200 }}>
-					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/category" element={<CategoryPage />} />
-						<Route path="/notification" element={<NotificationPage />} />
-						<Route path="/cart" element={<CartPage />} />
-						<Route path="/profile" element={<ProfilePage />} />
-						<Route path="/success" element={<SuccessPage />} />
-					</Routes>
-				</CSSTransition>
-			</TransitionGroup>
+		<React.Fragment>
 			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/category" element={<CategoryPage />} />
+				<Route path="/notification" element={<NotificationPage />} />
+				<Route path="/cart" element={<CartPage />} />
 				<Route path="/search" element={<SearchPage />} />
+				<Route path="/profile" element={<ProfilePage />} />
+				<Route path="/success" element={<SuccessPage />} />
 			</Routes>
-		</>
+		</React.Fragment>
 	);
 };
 
